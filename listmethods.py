756fddeed9f1aa436e9c683e23587ac870498dbe
mehakmazhar <109718083+mehakmazhar@users.noreply.github.com>
2023-02-20 17:15:21 +0500

    Add files via upload

---
+++ listmethods.py
@@ -0,0 +1,40 @@
+l = ['mehak','ali','ilyas']
+      #0        1     2       3
+      # clear
+
+#remove
+# a=l.remove('ilyas')
+# print(a)
+
+#pop
+# il=l.pop(3)
+# print(l)
+
+# remove vs pop
+'''
+1) remove value , pop index
+2) remove permanantly item , fetch partclar 
+
+
+'''
+
+#count
+
+
+#index
+
+
+
+#copy
+
+
+#REVERSE
+
+
+
+
+
+
+
+
+
